b6b77c33c8194e328ee14399601f1936cfab6524
Inteyaz <inteyazansari322@gmail.com>
2026-09-07 13:07:04 +0530

    configure db and basic express server

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
